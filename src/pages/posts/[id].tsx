12c05f526372568295d842cf0ccd50374682c043
lucas <lac.lucas96@gmail.com>
2022-10-15 15:40:13 -0300

    feat: add tailwindcss typography

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -6,41 +6,44 @@ import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 interface IPost {
-	postData: {
-		title: string;
-		date: string;
-		contentHtml: string;
-	};
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
 }
 
 export default function Post({ postData }: IPost) {
-	return (
-		<Layout>
-			<Head>
-				<title>{postData.title}</title>
-			</Head>
-			<h1 className="text-2xl font-bold">{postData.title}</h1>
-			<Date dateString={postData.date} />
-			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}></div>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <h1 className="font-bold text-2xl">{postData.title}</h1>
+      <Date dateString={postData.date} />
+      <article
+        className="prose"
+        dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
+      ></article>
+    </Layout>
+  );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-	const paths = getAllPostIds();
+  const paths = getAllPostIds();
 
-	return {
-		paths,
-		fallback: false,
-	};
+  return {
+    paths,
+    fallback: false,
+  };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-	const postData = await getPostData(params.id as string);
+  const postData = await getPostData(params.id as string);
 
-	return {
-		props: {
-			postData,
-		},
-	};
+  return {
+    props: {
+      postData,
+    },
+  };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,5 +16,8 @@ module.exports = {
   corePlugins: {
     fontSize: false,
   },
-  plugins: [require("tailwindcss-fluid-type")],
+  plugins: [
+    require("tailwindcss-fluid-type"),
+    require("@tailwindcss/typography"),
+  ],
 };
