bb4712048bbffd9d7377ce974025ae1b14bcc345
lucas <lac.lucas96@gmail.com>
2022-10-18 14:34:13 -0300

    feat: create Project section and card project component draft

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Layout, { siteTitle } from "../components/layout";
 import Date from "../components/date";
 import { getSortedPostsData } from "../lib/posts";
 import { GetStaticProps } from "next";
+import Image from "next/image";
 
 interface IHome {
   allPostsData: {
@@ -19,25 +20,55 @@ export default function Home({ allPostsData }: IHome) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className="text-center">
-        <p>Hello! My name is Lucas and I'm a Frontend developer</p>
-        <p>This is my personal blog where I post about my study notes</p>
-      </section>
-      <section>
-        <h2 className="mb-4 font-bold text-2xl">Blog</h2>
-        <ul className="flex flex-col gap-4">
-          {allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
-              <Link href={`/posts/${id}`}>
-                <a className="text-lg">{title}</a>
-              </Link>
-              <small className="block">
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
+      <div className="flex flex-col gap-8">
+        <section className="text-center">
+          <p>Hello! My name is Lucas and I'm a Frontend developer</p>
+          <p>This is my personal blog where I post about my study notes</p>
+        </section>
+        <section>
+          <h2 className="mb-4 font-bold text-2xl">Blog</h2>
+          <ul className="flex flex-col gap-4">
+            {allPostsData.map(({ id, date, title }) => (
+              <li key={id}>
+                <Link href={`/posts/${id}`}>
+                  <a className="text-lg">{title}</a>
+                </Link>
+                <small className="block">
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))}
+          </ul>
+        </section>
+        <section>
+          <h2 className="mb-4 font-bold text-2xl">Projects</h2>
+          <div className="flex flex-wrap gap-4">
+            <Link href="/">
+              <a className="flex max-w-md flex-col overflow-hidden rounded-md bg-white shadow-md hover:bg-zinc-300 hover:transition-colors">
+                <Image
+                  src="/assets/spacetourism.jpg"
+                  width={504}
+                  height={282}
+                />
+                <div className="flex flex-col p-4">
+                  <h3 className="text-lg dark:text-stone-900">
+                    Spacial Tourism Website
+                  </h3>
+                  <p className="text-xs dark:text-stone-900">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Iure, tenetur.
+                  </p>
+                  <ul className="flex gap-4 text-stone-900">
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                  </ul>
+                </div>
+              </a>
+            </Link>
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 }
